1d4ef59af4e52023924d6007434223225d7dac79
HOME-PC\aisir <aisir49@yandex.ru>
2022-11-14 13:07:05 +0300

    Исправлен код компонента

--- local/components/custom/working.days/class.php
+++ local/components/custom/working.days/class.php
@@ -13,40 +13,9 @@ class WorkingDays extends CBitrixComponent
             if ($arDate = $datesObj->Fetch()) {
                 $this->arResult["HOLIDAY_DATE"] = $_POST["date"];
             } else {
-                $arDate = explode(".", $_POST["date"]);
-                $year = (int) $arDate[2];
-                $month = (int) $arDate[1];
-                $day = (int) $arDate[0];
-                $year -= 2020;
-                $days = 365 * $year + intval($year/4) + 1;
-                switch ($month) {
-                    case 1: $days += $day;
-                        break;
-                    case 2: $days += $day + 31;
-                        break;
-                    case 3: $days += $day + 31 + 28;
-                        break;
-                    case 4: $days += $day + 31 + 28 + 31;
-                        break;
-                    case 5: $days += $day + 31 + 28 + 31 + 30;
-                        break;
-                    case 6: $days += $day + 31 + 28 + 31 + 30 + 31;
-                        break;
-                    case 7: $days += $day + 31 + 28 + 31 + 30 + 31 + 30;
-                        break;
-                    case 8: $days += $day + 31 + 28 + 31 + 30 + 31 + 30 + 31;
-                        break;
-                    case 9: $days += $day + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31;
-                        break;
-                    case 10: $days += $day + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30;
-                        break;
-                    case 11: $days += $day + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31;
-                        break;
-                    case 12: $days += $day + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30;
-                        break;
-                }
-                $days = $days % 7;
-                if ($days == 4 || $days == 5) {
+                $time = strtotime($_POST["date"]);
+                $dayOfWeek = date('w', $time);
+                if ($dayOfWeek == 6 || $dayOfWeek == 0) {
                     $this->arResult["HOLIDAY_DATE"] = $_POST["date"];
                     $this->arResult["HOLIDAY_DATE"] = $_POST["date"];
                 } else {
